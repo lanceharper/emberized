89e937fb40c48da4ff64d99725e39fcfa4b149d0
Colin Hicks <colin.hicks@gmail.com>
2013-05-22 11:18:18 -0400

    remove workaround note, obviated by new precompilation approach

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,4 @@
-define(['ember', 'templates'], function(Em, templates) {
-	// ember overwrites this value with a new obj, so we need to set it
-	// after ember is required.
-	Ember.TEMPLATES = templates;
-
+define(['ember', 'templates'], function(Em) {
 	// return the singleton app instance
 	return Em.Application.create({
 		LOG_TRANSITIONS: true
